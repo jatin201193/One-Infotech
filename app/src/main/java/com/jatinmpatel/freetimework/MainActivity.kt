66a949fa0e25bd70dc2bd09c97217af156e2dbf6
Jatin Patel <jatin201193patel@gmail.com>
2021-03-20 23:27:14 +0000

    hello

---
+++ app/src/main/java/com/jatinmpatel/freetimework/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.jatinmpatel.freetimework
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
